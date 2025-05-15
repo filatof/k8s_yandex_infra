f5c6290fd45dbc40ef2be111499f2c18bc0ea920
filatof <filatof@gmail.com>
2025-05-15 09:58:53 +0300

    add terraform infra

---
+++ terraform/provider.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">=0.75.0"
+    }
+  }
+  #---------загрузка файла состояний в s3-------------
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket                   = "terraform-state-standart"
+    region                   = "ru-central1"
+    key                      = "instance-group-k8s.tfstate"
+    shared_credentials_files = ["storage.key"] #ссылка на ключ доступа к бакету
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
+}
+
+provider "yandex" {
+  token     = var.yc_token
+  service_account_key_file = "key.json"
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+}
+
---
+++ terraform/vars.tf
@@ -0,0 +1,20 @@
+# Variables
+
+variable "yc_token" {
+  type        = string
+  description = "Yandex Cloud API key"
+  default     = "TOKEN_CHANGE_ME"
+}
+
+variable "yc_cloud_id" {
+  type        = string
+  description = "Yandex Cloud id"
+  default     = "b1givfjnecaq6gsd91ml"
+}
+
+variable "yc_folder_id" {
+  type        = string
+  description = "Yandex Cloud folder id"
+  default     = "b1g7qh7t0i4sftogmaue"
+}
+
